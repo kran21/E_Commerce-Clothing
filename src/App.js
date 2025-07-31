d876eb47851e1319856c9c845cbdba408d90e421
karan Patil <karan.patil2109@Gmail.com>
2025-07-31 19:26:46 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './customer/component/Navigation/Navigation';
+import { Homepage } from './customer/pages/Homepage/Homepage';
+
+
+function App() {
+  return (
+    <div className="">
+      <Navigation/>
+      <div>
+        <Homepage/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+
+
---
+++ src/customer/pages/Homepage/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainCarousel from '../../component/HomeCarousel/MainCarousel'
+
+export const Homepage = () => {
+  return (
+    <div>
+        <MainCarousel/>
+        <div>
+            other section
+        </div>
+    </div>
+  )
+}
