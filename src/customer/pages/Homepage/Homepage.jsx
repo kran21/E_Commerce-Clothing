c3927baf00932513e17b956b2ee17e217cae32c8
kran21 <karan.patil2109@Gmail.com>
2025-08-03 23:09:48 +0530

    Intial commit

--- src/customer/pages/Homepage/Homepage.jsx
+++ src/customer/pages/Homepage/Homepage.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import MainCarousel from '../../component/HomeCarousel/MainCarousel'
+import SectionCarousel from '../../component/HomeSectionCarousel/SectionCarousel'
 
 export const Homepage = () => {
   return (
     <div>
         <MainCarousel/>
         <div>
-            other section
+            <SectionCarousel/>
         </div>
     </div>
   )
