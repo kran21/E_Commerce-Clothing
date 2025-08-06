df31c36cd143926e8448a9ef1a77913d9e4411ea
kran21 <karan.patil2109@Gmail.com>
2025-08-06 17:16:02 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-import Navigation from './customer/component/Navigation/Navigation';
-import { Homepage } from './customer/pages/Homepage/Homepage';
-
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navigation from "./customer/Components/Navbar/Navigation";
+import CustomerRoutes from "./Routers/CustomerRoutes";
+import AdminRoutes from "./Routers/AdminRoutes";
+import NotFound from "./Pages/Notfound";
+import AdminPannel from "./Admin/AdminPannel";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getUser } from "./Redux/Auth/Action";
+// import Routers from './Routers/Routers';
 
 function App() {
+  const {auth}=useSelector(store=>store);
+  const dispatch = useDispatch();
+  const jwt = localStorage.getItem("jwt");
+  useEffect(() => {
+    if (jwt) {
+      dispatch(getUser(jwt));
+    }
+  }, [jwt]);
   return (
     <div className="">
-      <Navigation/>
-      <div>
-        <Homepage/>
-      </div>
+      <Routes>
+        <Route path="/*" element={<CustomerRoutes />} />
+       {auth.user?.role==="ROLE_ADMIN" && <Route path="/admin/*" element={<AdminPannel />} />}
+      </Routes>
     </div>
   );
 }
 
 export default App;
-
-
-
